a45fd190a6cb296dbd3dc47424060217fc7e5ef9
Merrick Cai <merrickcai.dev@gmail.com>
2026-01-24 22:07:19 -0800

    Updates Google Maps embed link for improved accuracy

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -99,7 +99,7 @@ export default function ContactPage() {
 
           <div className="rounded-2xl overflow-hidden border border-border">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2688.8584449!2d-122.1664!3d47.6291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906c8c1a2b1a3d%3A0x0!2s1433%20130th%20Ave%20NE%2C%20Bellevue%2C%20WA%2098005!5e0!3m2!1sen!2sus!4v1"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10756.905099105446!2d-122.18305072974243!3d47.621732650000105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906d65bd01c2d7%3A0xd67fcb221432b50f!2sNatural%20Beauty%20Spa!5e0!3m2!1sen!2sus!4v1769320078526!5m2!1sen!2sus"
               width="100%"
               height="450"
               style={{ border: 0 }}
